7de25bbd7161e50de9a3c2fc1960b19651070d02
Billy <desynahmed@gmail.com>
2024-12-08 00:00:48 +0100

    add Task1.java

---
+++ Task1.java
@@ -0,0 +1,9 @@
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Task 1: Print 1-10");
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
