9b22d4a8197b6d2f311bb3a9ff5b80e3ff6f9e5e
Billy <desynahmed@gmail.com>
2024-12-08 00:12:06 +0100

    add Task9.java

---
+++ Task9.java
@@ -0,0 +1,8 @@
+public class Task9 {
+    public static void main(String[] args) {
+        int result = 38812;
+        System.out.println("Task 9: Print the square of result from Task 7");
+        long square = (long) result * result; 
+        System.out.println("Square: " + square);
+    }
+}
