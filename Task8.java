07bbc22832eafa36db717f31a5fed999727d5ed6
Billy <desynahmed@gmail.com>
2024-12-08 00:09:42 +0100

    add Task8.java

---
+++ Task8.java
@@ -0,0 +1,7 @@
+public class Task8 {
+    public static void main(String[] args) {
+        int sum = 38812;
+        System.out.println("Task 8: Sum the pairs from Task 7 together");
+        System.out.println("Total Sum: " + sum);
+    }
+}
