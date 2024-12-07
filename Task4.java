ad76ef10e0c746b71eb69e1c83912d912d00381c
Billy <desynahmed@gmail.com>
2024-12-08 00:06:55 +0100

    add Task4.java

---
+++ Task4.java
@@ -0,0 +1,10 @@
+public class Task4 {
+    public static void main(String[] args) {
+        System.out.println("Task 4: Print multiples of 4 between 1 to 10");
+        int[] multiples = {4, 8};
+        for (int multiple : multiples) {
+            System.out.print(multiple + " ");
+        }
+        System.out.println();
+    }
+}
